7fd5264e4aa18940bda915572a053b87048638bf
Dmitriy <dimbaslv@gmail.com>
2019-06-11 00:14:51 -0500

    Updates

---
+++ precache-manifest.febb9cb33e19aa3ce6571526ff8e2291.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13b029294b04c1826b95dd89997909a8",
+    "url": "/react-clicky-game/index.html"
+  },
+  {
+    "revision": "9b218e500ffc7e6b3236",
+    "url": "/react-clicky-game/static/css/main.c2a839ca.chunk.css"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/react-clicky-game/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "9b218e500ffc7e6b3236",
+    "url": "/react-clicky-game/static/js/main.86c80e3d.chunk.js"
+  },
+  {
+    "revision": "a3b74089770802ae2f4d",
+    "url": "/react-clicky-game/static/js/runtime~main.a57faf00.js"
+  },
+  {
+    "revision": "542693aba203b7d2ae21e749a0b597d6",
+    "url": "/react-clicky-game/static/media/minniemouse.542693ab.jpg"
+  }
+]);
