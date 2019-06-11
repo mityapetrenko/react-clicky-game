2d62e6838d9670b66a477c35e476b535251460af
Dmitriy <dimbaslv@gmail.com>
2019-06-11 00:13:35 -0500

    working app

---
+++ src/components/Card/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Card.css";
+
+const Card = props => (
+  <div className="card" onClick={() => props.clickCount(props.id)}>
+    <div className="img-container">
+      <img alt={props.image.replace(".jpg", "")} src={require("../../images/"+ props.image)} />
+    </div>
+  </div>
+);
+
+export default Card;
